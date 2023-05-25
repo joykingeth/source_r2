409000e6091be63a423d23e7940d569fed28497e
Rick Lan <ricklan@gmail.com>
2023-05-25 10:56:11 +0800

    Moved OSM server to system/hardware/eon/libs/

--- launch_chffrplus.sh
+++ launch_chffrplus.sh
@@ -198,7 +198,7 @@ function two_init {
     if [ $dp_mapd == "1" ]; then
       MODULE="osm-3s_v0.7.56"
       if [ ! -d /data/media/0/osm/ ]; then
-        tar -vxf "/data/openpilot/selfdrive/mapd/assets/$MODULE.tar.xz" -C /data/media/0/
+        tar -vxf "/data/openpilot/system/hardware/eon/libs/$MODULE.tar.xz" -C /data/media/0/
         mv "/data/media/0/$MODULE" /data/media/0/osm
       fi
     fi
