5bd97c9c00368ddaddd63a339aeee30c4048c081
Rick Lan <ricklan@gmail.com>
2023-06-08 16:55:34 +0800

    removed translations in ui.h

--- selfdrive/ui/ui.h
+++ selfdrive/ui/ui.h
@@ -72,17 +72,17 @@ struct Alert {
       // Handle controls timeout
       if (sm.rcv_frame("controlsState") < started_frame) {
         // car is started, but controlsState hasn't been seen at all
-        return {QObject::tr("openpilot Unavailable"), QObject::tr("Waiting for controls to start"),
+        return {"openpilot Unavailable", "Waiting for controls to start",
                 "controlsWaiting", cereal::ControlsState::AlertSize::MID,
                 AudibleAlert::NONE};
       } else if (controls_missing > CONTROLS_TIMEOUT && !Hardware::PC()) {
         // car is started, but controls is lagging or died
         if (cs.getEnabled() && (controls_missing - CONTROLS_TIMEOUT) < 10) {
-          return {QObject::tr("TAKE CONTROL IMMEDIATELY"), QObject::tr("Controls Unresponsive"),
+          return {"TAKE CONTROL IMMEDIATELY", "Controls Unresponsive",
                   "controlsUnresponsive", cereal::ControlsState::AlertSize::FULL,
                   AudibleAlert::WARNING_IMMEDIATE};
         } else {
-          return {QObject::tr("Controls Unresponsive"), QObject::tr("Reboot Device"),
+          return {"Controls Unresponsive", "Reboot Device",
                   "controlsUnresponsivePermanent", cereal::ControlsState::AlertSize::MID,
                   AudibleAlert::NONE};
         }
