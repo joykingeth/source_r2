8000f41caa065954613d6c4943831b87e493f75e
Rick Lan <ricklan@gmail.com>
2023-09-13 12:14:21 +0800

    new de2e

--- selfdrive/controls/lib/dynamic_endtoend_controller.py
+++ selfdrive/controls/lib/dynamic_endtoend_controller.py
@@ -11,16 +11,20 @@ SLOW_DOWN_PROB = 0.6
 SLOW_DOWN_BP = [0., 10., 20., 30., 40., 50., 55.]
 SLOW_DOWN_DIST = [10, 30., 50., 70., 80., 90., 120.]
 
-STANDSTILL_SIZE = 5
-STANDSTILL_PROB = 0.4
-
 SLOWNESS_WINDOW_SIZE = 20
 SLOWNESS_PROB = 0.6
 SLOWNESS_CRUISE_OFFSET = 1.05
 
 DANGEROUS_TTC_WINDOW_SIZE = 5
-DANGEROUS_TTC = 1.6
+DANGEROUS_TTC = 1.55
 
+HIGHWAY_CRUISE_KPH = 75
+
+STOP_AND_GO_FRAME = 500
+class SNG_State:
+  off = 0
+  stopped = 1
+  going = 2
 
 class GenericMovingAverageCalculator:
   def __init__(self, window_size):
@@ -61,10 +65,6 @@ class DynamicEndtoEndController:
     self._has_blinkers = False
     self._has_blinkers_prev = False
 
-    self._standstill_gmac = GenericMovingAverageCalculator(window_size=STANDSTILL_SIZE)
-    self._has_standstill_filtered = False
-    self._has_standstill_filtered_prev = False
-
     self._slowness_gmac = GenericMovingAverageCalculator(window_size=SLOWNESS_WINDOW_SIZE)
     self._has_slowness = False
     self._has_slowness_prev = False
@@ -84,6 +84,9 @@ class DynamicEndtoEndController:
 
     self._has_standstill = False
     self._has_standstill_prev = False
+
+    self._sng_transit_frame = 0
+    self._sng_state = SNG_State.off
     pass
 
   def _update(self, car_state, lead_one, md, controls_state, radar_unavailable):
@@ -106,9 +109,19 @@ class DynamicEndtoEndController:
     # blinker detection
     self._has_blinkers = car_state.leftBlinker or car_state.rightBlinker
 
-    # standstill detection
-    self._standstill_gmac.add_data(car_state.standstill)
-    self._has_standstill_filtered = self._standstill_gmac.get_moving_average() >= STANDSTILL_PROB
+    # sng detection
+    if self._has_standstill:
+      self._sng_state = SNG_State.stopped
+      self._sng_transit_frame = 0
+    else:
+      if self._sng_transit_frame == 0:
+        if self._sng_state == SNG_State.stopped:
+          self._sng_state = SNG_State.going
+          self._sng_transit_frame = STOP_AND_GO_FRAME
+        elif self._sng_state == SNG_State.going:
+          self._sng_state = SNG_State.off
+      elif self._sng_transit_frame > 0:
+        self._sng_transit_frame -= 1
 
     # slowness detection
     self._slowness_gmac.add_data(self._v_ego_kph <= (self._v_cruise_kph*SLOWNESS_CRUISE_OFFSET))
@@ -126,28 +139,44 @@ class DynamicEndtoEndController:
 
 
     # keep prev values
+    self._has_standstill_prev = self._has_standstill
     self._has_slowness_prev = self._has_slowness
-    self._has_standstill_filtered_prev = self._has_standstill_filtered
     self._has_slow_down_prev = self._has_slow_down
     self._has_lead_filtered_prev = self._has_lead_filtered
 
   def _blended_priority_mode(self):
-    if self._has_blinkers and self._v_ego_kph < 60:
+    # when blinker is on and speed is driving below highway cruise speed: blended
+    # we dont want it to switch mode at higher speed, blended may trigger hard brake
+    if self._has_blinkers and self._v_ego_kph < HIGHWAY_CRUISE_KPH:
       self._mode = 'blended'
       return
 
+    # when at highway cruise and SNG: blended
+    # ensuring blended mode is used because acc is bad at catching SNG lead car
+    # especially those who accel very fast and then brake very hard.
+    if self._sng_state == SNG_State.going and self._v_cruise_kph >= HIGHWAY_CRUISE_KPH:
+      self._mode = 'blended'
+      return
+
+    # when standstill: blended
+    # in case of lead car suddenly move away under traffic light, acc mode wont brake at traffic light.
     if self._has_standstill:
       self._mode = 'blended'
       return
 
+    # when detecting slow down scenario: blended
+    # e.g. traffic light, curve, stop sign etc.
     if self._has_slow_down:
       self._mode = 'blended'
       return
 
+    # when detecting lead slow down: blended
+    # use blended for higher braking capability
     if self._has_dangerous_ttc:
       self._mode = 'blended'
       return
 
+    # car driving at speed lower than set speed: acc
     if self._has_slowness:
       self._mode = 'acc'
       return
