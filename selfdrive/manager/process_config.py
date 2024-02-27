215f114a611afb4373d1ac147362ab0a7249426d
Rick Lan <ricklan@gmail.com>
2024-02-27 11:45:30 +0800

    only for offroad

--- selfdrive/manager/process_config.py
+++ selfdrive/manager/process_config.py
@@ -93,8 +93,8 @@ procs = [
   # gpxd
   # PythonProcess("gpxd", "selfdrive.dragonpilot.gpxd"),
   # PythonProcess("gpx_uploader", "selfdrive.dragonpilot.gpx_uploader", offroad=True),
-  NativeProcess("otisserv", "selfdrive/dragonpilot", ['./otisserv'], offroad=True),
-  NativeProcess("fileserv", "selfdrive/dragonpilot", ['./fileserv'], offroad=True),
+  NativeProcess("otisserv", "selfdrive/dragonpilot", ['./otisserv'], onroad=False, offroad=True),
+  NativeProcess("fileserv", "selfdrive/dragonpilot", ['./fileserv'], onroad=False, offroad=True),
 ]
 
 managed_processes = {p.name: p for p in procs}
